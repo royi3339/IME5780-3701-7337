eddedeb9fb47abe4083c0edebb45d579ec64b2ea
royi3 <royi3@DESKTOP-E9L3AU8>
2020-04-02 18:57:10 +0300

    sheela 2 almost done!

---
+++ src/unittests/SphereTests.java
@@ -0,0 +1,54 @@
+package unittests;
+
+import geometries.Sphere;
+import org.junit.Test;
+import primitives.Point3D;
+import primitives.Vector;
+
+import static org.junit.Assert.*;
+
+/**
+ * implements the SphereTests class.
+ *
+ * @author Royi Alishayev idan darmoni
+ */
+public class SphereTests {
+
+    /**
+     * test method for
+     * {@link Sphere#Sphere(Point3D, double)}
+     */
+    @Test
+    public void testConstructor() {
+        // TC01: negative radius
+        try {
+            new Sphere(new Point3D(1, 1, 1), -5);
+            fail("Constructed a Sphere with a negative radius");
+        } catch (IllegalArgumentException e) {}
+
+        // TC02: zero radius
+        try {
+            new Sphere(new Point3D(1, 1, 1), 0);
+            fail("Constructed a Sphere with a ZERO radius");
+        } catch (IllegalArgumentException e) {}
+
+
+        // TC03: correct Sphere
+        try {
+            new Sphere(new Point3D(1, 2, 3), 4);
+        } catch (IllegalArgumentException e) {
+            fail("Failed constructing a correct Sphere");
+        }
+    }
+
+    /**
+     * test method for
+     * {@link Sphere#getNormal(Point3D)}
+     */
+    @Test
+    public void testGetNormal() {
+        // TC01: check the normal
+        Sphere s = new Sphere(new Point3D(1, 1, 1), 5);
+        assertEquals(" Bad normal to Sphere ", new Vector(0, 0, 1), s.getNormal(new Point3D(1, 1, 6)));
+    }
+}
