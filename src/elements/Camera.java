6916a2f2a2b66cd35beae95f3282434550eae269
royi3 <royi3@DESKTOP-E9L3AU8>
2020-05-03 19:49:51 +0300

    sheela 4 done!

---
+++ src/elements/Camera.java
@@ -0,0 +1,81 @@
+package elements;
+
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import static primitives.Util.isZero;
+
+/**
+ * implements the Camera class.
+ *
+ * @author Royi Alishayev idan darmoni
+ */
+public class Camera {
+    private Point3D _p0;
+    private Vector _vUp;
+    private Vector _vTo;
+    private Vector _vRight;
+
+    /**
+     * @param l  <b> the location Point3D of the Camera </b>
+     * @param up <b> the up Vector of the Camera </b>
+     * @param to <b> the toward Vector of the Camera </b>
+     */
+    public Camera(Point3D l, Vector to, Vector up) {
+        _p0 = new Point3D(l);
+        if (!isZero(up.dotProduct(to)))
+            throw new IllegalArgumentException("Error ! ! ! the Vectors not orthogonal !");
+        _vUp = up.normalized();
+        _vTo = to.normalized();
+        _vRight = up.crossProduct(to).scale(-1).normalized();
+    }
+
+    /**
+     * @return Point3D <b> the location Point3D of the Camera </b>
+     */
+    public Point3D getP() { return _p0; }
+
+    /**
+     * @return Vector <b> the up Vector of the Camera </b>
+     */
+    public Vector getVup() { return _vUp; }
+
+    /**
+     * @return Vector <b> the toward Vector of the Camera </b>
+     */
+    public Vector getVto() { return _vTo; }
+
+    /**
+     * @return Vector <b> the right Vector of the Camera </b>
+     */
+    public Vector getVright() { return _vRight; }
+
+    /**
+     * to write...
+     *
+     * @param nX             <b> the size of the X axis </b>
+     * @param nY             <b> the size of the Y axis </b>
+     * @param j              <b> the value of the X index </b>
+     * @param i              <b> the value of the Y index </b>
+     * @param screenDistance <b> the distance from the camera till the view plane </b>
+     * @param screenWidth    <b> the width screen of the view plane </b>
+     * @param screenHeight   <b> the height screen of the view plane </b>
+     * @return Ray <b> the Ray that goes through the pixel </b>
+     */
+    public Ray constructRayThroughPixel(int nX, int nY, int j, int i, double screenDistance, double screenWidth, double screenHeight) {
+        if (screenDistance < 0 || isZero(screenDistance)) {
+            throw new IllegalArgumentException("Error ! ! ! the screenDistance, should be a positive number (screenDistance > 0)");
+        }
+        Point3D pCenter = _p0.add(_vTo.scale(screenDistance));
+        double rY = screenHeight / nY;
+        double rX = screenWidth / nX;
+        double yI = (i - (double) nY / 2) * rY + rY / 2;
+        double xJ = (j - (double) nX / 2) * rX + rX / 2;
+        Point3D p = new Point3D(pCenter);
+        if (!isZero(xJ)) { p = p.add(_vRight.scale(xJ)); }
+        if (!isZero(yI)) { p = p.add(_vUp.scale(-yI)); }
+        Vector v = p.subtract(_p0);
+        return new Ray(_p0, v);
+    }
+}
---
+++ src/unittests/IntegrationTests.java
@@ -0,0 +1,75 @@
+package unittests;
+
+import static org.junit.Assert.*;
+
+import elements.Camera;
+import geometries.*;
+import org.junit.Test;
+import primitives.Point3D;
+
+import primitives.Ray;
+import primitives.Vector;
+
+public class IntegrationTests {
+    /**
+     * Test method for Integration Tests.
+     */
+    @Test
+    public void testRayIntersection() {
+        Camera camera;
+
+        // **** Group: Ray's intersections test with Sphere
+        // TC01: Sphere test r = 1, with 2 intersections at the center Ray
+        camera = new Camera(new Point3D(Point3D.ZERO), new Vector(0, 0, 1), new Vector(0, -1, 0));
+        assertEquals("TC01: should be 2 intersections with the Sphere !", 2, sumOfIntersections(camera, new Sphere(new Point3D(0, 0, 3), 1)));
+        // TC02: Sphere test r = 2.5, with 18 intersections, enter and exit of all of the Rays
+        camera = new Camera(new Point3D(0, 0, -0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
+        assertEquals("TC02: should be 18 intersections with the Sphere !", 18, sumOfIntersections(camera, new Sphere(new Point3D(0, 0, 2.5), 2.5)));
+        // TC03: Sphere test r = 2, with 10 intersections, enter and exit of all of the Ray excluded the corners
+        assertEquals("TC03: should be 10 intersections with the Sphere !", 10, sumOfIntersections(camera, new Sphere(new Point3D(0, 0, 2), 2)));
+        // TC04: Sphere test r = 4, with 9 intersections exit for all of the Rays
+        assertEquals("TC04: should be 9 intersections with the Sphere !", 9, sumOfIntersections(camera, new Sphere(new Point3D(0, 0, 1), 4)));
+        // TC05: Sphere test r = 0.5, with 0 intersections, the Sphere is behind the camera
+        assertEquals("TC05: should not be any intersections with the Sphere !", 0, sumOfIntersections(camera, new Sphere(new Point3D(0, 0, -1), 0.5)));
+
+        // **** Group: Ray's intersections test with Plane
+        // TC11: the plane is parallel to the view plane and toward to the camera, with 9 intersections
+        assertEquals("TC11: should be 9 intersections with the Plane !", 9, sumOfIntersections(camera, new Plane(new Point3D(0, 0, 3), new Vector(0, 0, 1))));
+        // TC12: the plane is not parallel to the view plane and toward to the camera, with 9 intersections
+        assertEquals("TC12: should be 9 intersections with the Plane !", 9, sumOfIntersections(camera, new Plane(new Point3D(0, 0, 10), new Vector(0, -0.5, 1))));// שיניתי את ט מ -2 ל -0.5
+        //TC13: the plane is not parallel to the view plane and toward to the camera, with 6 intersections
+        assertEquals("TC13: should be 6 intersections with the Plane !", 6, sumOfIntersections(camera, new Plane(new Point3D(0, 0, 10), new Vector(0, -1, 1))));
+
+        // **** Group: Ray's intersections test with Triangle
+        // TC21: the triangle is parallel to the view plane and has 1 intersection
+        assertEquals("TC21: should be 1 intersection with the Triangle !", 1, sumOfIntersections(camera, new Triangle(new Point3D(0, -1, 2), new Point3D(1, 1, 2), new Point3D(-1, 1, 2))));
+        // TC22: the triangle is parallel to the view plane with 2 intersections
+        assertEquals("TC22: should be 2 intersections with the Triangle !", 2, sumOfIntersections(camera, new Triangle(new Point3D(0, -20, 2), new Point3D(1, 1, 2), new Point3D(-1, 1, 2))));
+    }
+
+    /**
+     * this method work only with 3x3 matrix.
+     * <p>
+     * HxW = 3x3.
+     * <p>
+     * the distance = 1.
+     * <p>
+     * this method using the constructRayThroughPixel() function.
+     *
+     * @param camera <b> the given Camera </b>
+     * @param geoObj <b> the geometries object that we checking the intersections with </b>
+     * @return int <b> the sum of the intersections, with the Ray's Camera </b>
+     */
+    public int sumOfIntersections(Camera camera, Geometry geoObj) {
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Ray ray = camera.constructRayThroughPixel(3, 3, i, j, 1, 3, 3);
+                if (geoObj.findIntersections(ray) != null) {
+                    sum += geoObj.findIntersections(ray).size();
+                }
+            }
+        }
+        return sum;
+    }
+}
