856dcc87a2c4d12da8fb2c8cf8665228ebe01766
royi3 <royi3@DESKTOP-E9L3AU8>
2020-06-08 20:11:34 +0300

    sheela 7 done!

--- src/unittests/ReflectionRefractionTests.java
+++ src/unittests/ReflectionRefractionTests.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package unittests;
 
 import geometries.Plane;
@@ -20,8 +17,7 @@ import java.util.List;
  * Tests for reflection and transparency functionality, test for partial shadows
  * (with transparency)
  *
- * @author dzilb
- *
+ * @author dzilb && Royi Alishayev idan darmoni
  */
 public class ReflectionRefractionTests {
 
@@ -88,7 +84,7 @@ public class ReflectionRefractionTests {
     public void trianglesTransparentSphere() {
         Scene scene = new Scene("Test scene");
         scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
-        scene.setDistance(10000);
+        scene.setDistance(1000);
         scene.setBackground(Color.BLACK);
         scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
 
@@ -116,17 +112,18 @@ public class ReflectionRefractionTests {
     @Test
     public void numOfObjectsTest() {
 
-        Scene scene = new Scene("Object Test scene");
+        Scene scene = new Scene("Objects Test scene");
         scene.setDistance(10000);
         scene.setBackground(Color.BLACK);
         scene.setAmbientLight(new AmbientLight(new Color(255, 255, 255), 0.1));
 
-        Camera c1 = new Camera(new Point3D(-150, -4620, 90), new Vector(1300, 5800, 150), new Vector(-5800, 1300, 0));
-        Camera c2 = new Camera(new Point3D(0, -450, -7000), new Vector(20, 7, 100), new Vector(-100, 0, 20));
-        Camera c3 = new Camera(new Point3D(0, -450, -5900), new Vector(20, 200, 100), new Vector(-100, 0, 20));
-
+        //          creating 3 Cameras with others looking view, for the bonus
+        Camera c1 = new Camera(new Point3D(-150, -4620, 90), new Vector(1300, 5800, 150), new Vector(-5800, 1300, 0)); // picture #1
+        Camera c2 = new Camera(new Point3D(0, -450, -7000), new Vector(20, 7, 100), new Vector(-100, 0, 20)); // picture #2
+        Camera c3 = new Camera(new Point3D(0, -450, -5900), new Vector(20, 200, 100), new Vector(-100, 0, 20)); // picture #3
         List<Camera> cameraList = List.of(c1, c2, c3);
 
+        //          creating 5 Point3D to the pyramid with a Square base.
         Point3D p1 = new Point3D(850, -750, -200);
         Point3D p2 = new Point3D(850, -750, -400);
         Point3D p3 = new Point3D(850, -950, -400);
@@ -134,33 +131,35 @@ public class ReflectionRefractionTests {
         Point3D p5 = new Point3D(950, -850, -300);
 
         scene.addGeometries(
+                //          creating a 3 Spheres which the third Sphere is a mirror.
                 new Sphere(new Color(0, 0, 0), new Material(0.25, 0.25, 20, 0.7, 0), 200, new Point3D(900, -850, -300))    // אפור
                 , new Sphere(new Color(95, 0, 50), new Material(0.25, 0.75, 20, 0.5, 0), 200, new Point3D(900, -1600, 0))    // וורוד
                 , new Sphere(new Color(java.awt.Color.darkGray), new Material(0, 0.25, 20, 0, 1), 400, new Point3D(1100, 0, 200))    //מראה
 
+                //          creating a 4 infinity mirrors.
                 , new Plane(new Color(20, 20, 20), new Material(0.5, 0, 0, 0, 0.7), new Point3D(0, 0, 0), new Vector(0, 1, 0)) // קדמי
                 , new Plane(new Color(40, 40, 40), new Material(0, 0, 0, 0, 1), new Point3D(0, 0, 200), new Vector(0, 0, -1)) // צדדי
                 , new Plane(new Color(20, 20, 20), new Material(0, 0, 0, 0, 1), new Point3D(1000, -4621, 0), new Point3D(-800, -4621, 1000), new Point3D(0, -4621, 0)) // אחורי
                 , new Plane(new Color(java.awt.Color.black), new Material(0.2, 0, 20, 0, 0.2), new Point3D(1100, 0, 0), new Vector(-1, 0, 0)) // תחתון
 
+                //          creating a pyramid with a Square base.
                 , new Polygon(new Color(java.awt.Color.GREEN), new Material(0.25, 0.75, 20, 0.5, 0), p1, p2, p3, p4)
                 , new Triangle(new Color(java.awt.Color.YELLOW), new Material(0.25, 0.75, 20, 0.5, 0), p1, p2, p5)
                 , new Triangle(new Color(java.awt.Color.RED), new Material(0.25, 0.75, 20, 0.5, 0), p2, p3, p5)
                 , new Triangle(new Color(java.awt.Color.BLUE), new Material(0.25, 0.75, 20, 0.5, 0), p3, p4, p5)
                 , new Triangle(new Color(java.awt.Color.GREEN), new Material(0.25, 0.75, 20, 0.5, 0), p4, p1, p5)
         );
-
         scene.addLights(new DirectionalLight(new Color(255, 255, 255), new Vector(1, 0, 0)));
-
-        imagesWriter4K(true, cameraList, scene);
+        // helper method.
+        imagesWriter4K(false, cameraList, scene);
     }
 
     /**
-     * helper method for the test which checking if we want the pictures in 4K resolution.
+     * helper method for the test which checking if we want the pictures in 4K resolution or not.
      *
-     * @param b4K <b> true = 4K resolution, false = regular resolution </b>
+     * @param b4K        <b> true = 4K resolution, false = regular resolution </b>
      * @param cameraList <b> the List of the {@link Camera}s </b>
-     * @param scene <b> the {@link Scene} of our numOfObjectsTest </b>
+     * @param scene      <b> the {@link Scene} of our numOfObjectsTest </b>
      */
     private void imagesWriter4K(boolean b4K, List<Camera> cameraList, Scene scene) {
         String str;
